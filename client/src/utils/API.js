36de0af821baa422df0e83d5db1db9c62cc97f5f
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-04-02 17:53:59 -0700

    updated token and typdef resolvers

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,24 +1,28 @@
-var Yelp = require('yelp');
+// var Yelp = require('yelp');
 
-var yelp = new Yelp({
-  consumer_key: 'consumer-key',
-  consumer_secret: 'consumer-secret',
-  token: 'token',
-  token_secret: 'token-secret',
-});
+// var yelp = new Yelp({
+//   consumer_key: 'consumer-key',
+//   consumer_secret: 'consumer-secret',
+//   token: 'token',
+//   token_secret: 'token-secret',
+// });
 
-yelp.business("blackowned")
-  .then(console.log)
-  .catch(console.error);
+// yelp.business(query)
+//   .then(console.log)
+//   .catch(console.error);
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+
+
+
+
+
+ // https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
 
 
 // export const searchYelpBo = (query2) => {
-//   return fetch(`https://api.yelp.com/v3/businesses/search?term=${query2}&latitude=37.786882&longitude=-122.399972`)
+//   return yelp.business(query);
 // }
 
