e180d70e056c349be7d7a98f72a6c6fffac208c1
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-04-04 00:26:48 -0700

    updated queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -30,39 +30,3 @@ export const ADD_USER = gql`
     }
   }
 `;
-// export the save book mutation by their dependencies
-export const SAVE_BOOK = gql`
-  mutation saveBook($input: SavedBookInput) {
-    saveBook(input: $input) {
-      username
-      _id
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        image
-        link
-        title
-        description
-      }
-    }
-  }
-`;
-// export the removeBook by their dependencies
-export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: String!) {
-    removeBook(bookId: $bookId) {
-      _id
-      username
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        image
-        link
-        title
-        description
-      }
-    }
-  }
-`;
