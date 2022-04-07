9118642b50776d91ee77dbd891feebcf18edbb95
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-04-06 20:02:01 -0700

    updated about

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -8,9 +8,11 @@ function About() {
     <div className='container image-fluid'>
         <img className='animate__animated animate__bounce ' src={logo} style={{ width: "65%" }} />
         <h1>What's Good - An unapologetic experience</h1>
-        <p>I'm baby four loko labore fugiat flannel, aute slow-carb forage. Tempor listicle VHS duis tofu 
-          ut gochujang consectetur air plant fam gentrify. Vaporware stumptown photo booth kogi reprehenderit 
-          pinterest anim. Next level duis cillum quis cray vexillologist bespoke.</p>
+        <p>Make the best choice in deciding where to invest your time and money with our Whats-Good mobileapp. It allows BIPOC clients to view and post 
+          reviews about their experiences through the lenses of Black people. Our app is to bring awareness to Black peoples' experiences, whether they be 
+          positive or negative. In doing so, other Black people and allies can be aware of businesses' ethics, customer service and more to ensure a beneficial 
+          experience overall. Whats-Good mobile app is designed with tools to give Black people foresight into what type of businesses they are will
+           giving their resources to more confidently.</p>
         <button type='submit' className='btn-wg' size='lg'>SAY LESS</button>
     </div>
   )
