79b60d7520448e3faf0cfa073cf6ecf802394804
Kat Contreras <katcontreras@Kats-MacBook-Pro.local>
2022-04-08 11:05:16 -0700

    updated components

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -13,7 +13,7 @@ function About() {
           positive or negative. In doing so, other Black people and allies can be aware of businesses' ethics, customer service and more to ensure a beneficial 
           experience overall. <br></br><br></br>Whats-Good mobile app is designed with tools to give Black people foresight into what type of businesses they are will
            giving their resources to more confidently.</p>
-        <button type='submit' className='btn-wg' ><h2 as={Link} to='/search'>SAY LESS</h2></button>
+        <button type='submit' className='btn-wg'><h2 as={Link} to='/search'>SAY LESS</h2></button>
     </div>
   )
 }
